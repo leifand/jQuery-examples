ecf6fdcd4bbd2cfd250e01e719370638b4970910
Leif Anderson <leif.v.anderson@gmail.com>
2017-07-07 20:05:48 -0500

    disappearing ninja exercise added

--- javascript/disappear.js
+++ javascript/disappear.js
@@ -1,7 +1,13 @@
 /*  disappear.js
     Leif Anderson 7/4/17
 */
+$(document).ready(function() {
 
-$( document ).ready(function() {
+  $('img').click(function() {
+    $(this).hide();
+  });
 
-}
+  $('#save-the-bananas').click(function() {
+    $('img').show();
+  });
+});
